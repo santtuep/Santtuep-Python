2f2839db47c734e82b5dbe8fad7c1d7fc69bdc50
Santtuep <santtu.peltomaki@metropolia.fi>
2025-08-27 19:55:37 +0300

    New File Testing

---
+++ Python-peruskurssi/laskin.py
@@ -0,0 +1,7 @@
+a = float(input("Anna ensimmäinen luku: "))
+b = float(input("Anna toinen luku: "))
+
+print("Yhteenlasku:", a + b)
+print("Vähennyslasku", a - b)
+print("Kertolasku", a * b)
+print("Jakolasku", a / b)
